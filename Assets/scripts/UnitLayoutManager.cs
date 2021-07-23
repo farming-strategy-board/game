9fb3d8d1595299308881b6bf78cb69670485bbc9
NaYoung1017 <87241195+NaYoung1017@users.noreply.github.com>
2021-07-23 14:19:16 +0900

    UnitPlaceLayer.(2)
    2021.07.23
    유닛을 클릭하여 이동하는 기능까지 구현되어 있습니다.

--- Assets/scripts/UnitLayoutManager.cs
+++ Assets/scripts/UnitLayoutManager.cs
@@ -10,6 +10,7 @@ public class UnitLayoutManager : MonoBehaviour
   
     void Start()
     {
+        UnitLayoutPanel.SetActive(false);
         // 5초 로딩이후 유닛 배치 창 등장. 5초 로딩은 매칭이나 재화지급의 과정을 포함.
         StartCoroutine(DelayTime(5));
     }
